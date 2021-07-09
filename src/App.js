249ee40a290fb0e06ef75e56328d5227352ff649
Xumora <kh.mamirova@gmail.com>
2021-07-09 20:23:56 +0500

    title ozgardi

--- src/App.js
+++ src/App.js
@@ -4,20 +4,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React bo'yicha 1 -loyiha
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row py-5">
+          <div className="col-4">
+            <div className="box p-5 pt-2">
+              <h1 className="text-center display-1 fw-bold">,,</h1>
+              <p className="text-center text-white fw-bold">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptate quos architecto, laborum animi ex excepturi, odit quisquam vel, nulla dolorum illum suscipit!
+              </p>
+              <img src="https://anime-fans.ru/wp-content/uploads/2021/05/Krutoe-anime-magicheskaya-bitva-manga-kartinki_20.jpg" alt="?" className="rounded-circle d-block mx-auto ava" />
+              <h4 className="text-center">John Doe</h4>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="box p-5 pt-2">
+              <h1 className="text-center display-1 fw-bold">,,</h1>
+              <p className="text-center text-white fw-bold">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptate quos architecto, laborum animi ex excepturi, odit quisquam vel, nulla dolorum illum suscipit!
+              </p>
+              <img src="https://anime-fans.ru/wp-content/uploads/2021/05/Krutoe-anime-magicheskaya-bitva-manga-kartinki_20.jpg" alt="?" className="rounded-circle d-block mx-auto ava" />
+              <h4 className="text-center">John Doe</h4>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="box p-5 pt-2">
+              <h1 className="text-center display-1 fw-bold">,,</h1>
+              <p className="text-center text-white fw-bold">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptate quos architecto, laborum animi ex excepturi, odit quisquam vel, nulla dolorum illum suscipit!
+              </p>
+              <img src="https://anime-fans.ru/wp-content/uploads/2021/05/Krutoe-anime-magicheskaya-bitva-manga-kartinki_20.jpg" alt="?" className="rounded-circle d-block mx-auto ava" />
+              <h4 className="text-center">John Doe</h4>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
